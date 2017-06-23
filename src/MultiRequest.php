adf32495af67f75404dcd36992ef4966fcfabefe
吴强健 <qiangjian@staff.sina.com.cn>
2017-06-23 22:19:31 +0800

    =travis

--- src/MultiRequest.php
+++ src/MultiRequest.php
@@ -71,7 +71,7 @@ class MultiRequest {
 	public function execute() {
 		$sleepTime = 1000;//microsecond, prevent  CPU 100%
         //prepare conf
-		while (($multiFlg = curl_multi_exec(self::$multiHandler, $active)) == CURLM_CALL_MULTI_PERFORM);
+//		while (($multiFlg = curl_multi_exec(self::$multiHandler, $active)) == CURLM_CALL_MULTI_PERFORM);
         //fetch data
 //        while ($active && $multiFlg == CURLM_OK) {
 //            if (curl_multi_select(self::$multiHandler) != -1) {
